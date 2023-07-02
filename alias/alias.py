76e9790cb2e924a572786054f812adaac9a7f0bf
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-02 23:13:54 +1000

    Refactored code into functions

--- alias/alias.py
+++ alias/alias.py
@@ -1,28 +1,39 @@
 import os
-from constant import BASH_RC_FILE_NAME, BASH_ALIAS_FILE_NAME
+import sys
+
+BASH_RC_FILE_NAME = ".bashrc"
+BASH_ALIAS_FILE_NAME = ".shell-sobriquet-aliases.sh"
 
 home_dir = os.path.expanduser("~")
 bash_rc_file_path = f"{home_dir}/{BASH_RC_FILE_NAME}"
 bash_alias_file_path = f"{home_dir}/{BASH_ALIAS_FILE_NAME}"
+contents = f"\n\nif [ -f ~/{BASH_ALIAS_FILE_NAME} ]; then\n" \
+           f"\t. ~/{BASH_ALIAS_FILE_NAME}\n" \
+           "fi\n"
 
-if os.path.exists(bash_rc_file_path):
-    with open(bash_rc_file_path) as file:
-        if BASH_ALIAS_FILE_NAME not in file.read():
-            file.close()
-            with open(bash_rc_file_path, "a") as file:
-                lines = f"\n\nif [ -f ~/{BASH_ALIAS_FILE_NAME} ]; then\n" \
-                        f"\t. ~/{BASH_ALIAS_FILE_NAME}\n" \
-                        "fi\n"
-                file.write(lines)
-                file.close()
-else:
-    with open(bash_rc_file_path, "a") as file:
-        lines = f"\n\nif [ -f ~/{BASH_ALIAS_FILE_NAME} ]; then\n" \
-                f"\t. ~/{BASH_ALIAS_FILE_NAME}\n" \
-                "fi\n"
-        file.write(lines)
+def check_file_contains_string(file_path: str, phrase: str) -> bool:
+    if phrase in open(file_path).read():
+        return True
+    return False
+
+def write_string_to_file(file_path: str, contents: str, mode: str) -> None:
+    with open(file_path, mode) as file:
+        file.write(contents)
         file.close()
 
-if not os.path.exists(bash_rc_file_path):
-    open(f"{home_dir}/{BASH_ALIAS_FILE_NAME}", 'a').close()
+def register_alias_script() -> None:
+    if os.path.exists(bash_rc_file_path):
+        if check_file_contains_string(bash_rc_file_path, BASH_ALIAS_FILE_NAME):
+            write_string_to_file(bash_rc_file_path, contents, "a")
+    else:
+        write_string_to_file(bash_rc_file_path, contents, "a")
 
+def create_empty_alias_script() -> None:
+    if not os.path.exists(bash_rc_file_path):
+        open(f"{home_dir}/{BASH_ALIAS_FILE_NAME}", "a").close()
+
+def main():
+    cmd_args = sys.argv
+    print(cmd_args)
+    alias = cmd_args[1]
+    sh_command = cmd_args[2]
--- alias/constant.py
+++
@@ -1,2 +0,0 @@
-BASH_RC_FILE_NAME = ".bashrc"
-BASH_ALIAS_FILE_NAME = ".shell-sobriquet-aliases.sh"
