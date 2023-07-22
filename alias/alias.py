88f441926a9ed8742c9b7d322face7f3c0c41fae
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-22 21:43:24 +1000

    Added pydocs

--- alias/alias.py
+++ alias/alias.py
@@ -32,27 +32,51 @@ def check_file_contains_string(file_path: str, phrase: str) -> bool:
     return False
 
 def write_string_to_file(file_path: str, contents: str, mode: str) -> None:
+    """
+    This function writes a given string to file.
+
+    Parameters
+    ----------
+    :param file_path: The file path to write string to
+    :param contents: The string to be written
+    :param mode: The write mode
+    """
+
     with open(file_path, mode) as file:
         file.write(contents)
         file.close()
 
-def register_alias_script() -> None:
-    if os.path.exists(bash_rc_file_path):
-        if check_file_contains_string(bash_rc_file_path, BASH_ALIAS_FILE_NAME):
-            write_string_to_file(bash_rc_file_path, contents, "a")
-    else:
-        write_string_to_file(bash_rc_file_path, contents, "a")
-
 def create_empty_alias_script() -> None:
+    """
+    This function creates an empty shell file for aliases.
+    """
+
     if not os.path.exists(bash_rc_file_path):
         open(f"{home_dir}/{BASH_ALIAS_FILE_NAME}", "a").close()
 
 def register_alias(alias: str, sh_command: str) -> None:
+    """
+    This is a wrapper function to write aliases to shell file.
+
+    Parameters
+    ----------
+    :param alias: The alias to be written to file
+    :param sh_command: The shell command to be written to file
+    """
+
     alias_str = f'\n\nalias {alias}="{sh_command}"'
     write_string_to_file(bash_alias_file_path, alias_str, "a")
     subprocess.Popen(f"source ~/{BASH_ALIAS_FILE_NAME}", shell = True)
 
 def validate_input_args(input_args: list) -> bool:
+    """
+    This function validates input arguments.
+
+    Parameters
+    ----------
+    :param input_args: The input arguments to validate
+    """
+
     validated = True
     if len(input_args) != 3:
         print("Invalid number of input arguments provided.")
@@ -60,6 +84,10 @@ def validate_input_args(input_args: list) -> bool:
     return validated
 
 def main():
+    """
+    This main entry point function.
+    """
+
     cmd_args = sys.argv
     if validate_input_args(cmd_args):
         register_alias(cmd_args[1], cmd_args[2])
