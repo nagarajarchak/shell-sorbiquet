bda5da7e4f6e1ab5992ebdd4ed153f6859140732
Nagaraj Archak <archak.nagaraj@gmail.com>
2023-07-31 12:43:29 +1000

    Create a utility file + update whl file (#5)
    * A minor bug fix
    
    * Moved common function to utility
    
    * Created a utility file
    
    * Updated dist whl file

--- alias/alias.py
+++ alias/alias.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import subprocess
-import constant as c
+import alias.constant as c
+import alias.utility as u
 
 home_dir = os.path.expanduser("~")
 bash_rc_file_path = f"{home_dir}/{c.BASH_RC_FILE_NAME}"
@@ -10,47 +11,13 @@ contents = f"\n\nif [ -f ~/{c.BASH_ALIAS_FILE_NAME} ]; then\n" \
            f"\t. ~/{c.BASH_ALIAS_FILE_NAME}\n" \
            "fi\n"
 
-def check_file_contains_string(file_path: str, phrase: str) -> bool:
-    """
-    This function checks if a given string is present in the
-    given file path.
-
-    Parameters
-    ----------
-    :param file_path: The file path to check for string
-    :param phrase: The string phrase to check for existance
-
-    Returns
-    -------
-    A boolean indicating if the string is present or not
-    """
-
-    if phrase in open(file_path).read():
-        return True
-    return False
-
-def write_string_to_file(file_path: str, contents: str, mode: str) -> None:
-    """
-    This function writes a given string to file.
-
-    Parameters
-    ----------
-    :param file_path: The file path to write string to
-    :param contents: The string to be written
-    :param mode: The write mode
-    """
-
-    with open(file_path, mode) as file:
-        file.write(contents)
-        file.close()
-
 def create_empty_alias_script() -> None:
     """
     This function creates an empty shell file for aliases.
     """
 
     if not os.path.exists(bash_rc_file_path):
-        open(f"{home_dir}/{BASH_ALIAS_FILE_NAME}", "a").close()
+        open(f"{home_dir}/{c.BASH_ALIAS_FILE_NAME}", "a").close()
 
 def register_alias(alias: str, sh_command: str) -> None:
     """
@@ -63,8 +30,8 @@ def register_alias(alias: str, sh_command: str) -> None:
     """
 
     alias_str = f'\n\nalias {alias}="{sh_command}"'
-    write_string_to_file(bash_alias_file_path, alias_str, "a")
-    subprocess.Popen(f"source ~/{BASH_ALIAS_FILE_NAME}", shell = True)
+    u.write_string_to_file(bash_alias_file_path, alias_str, "a")
+    subprocess.Popen(f"source ~/{c.BASH_ALIAS_FILE_NAME}", shell = True)
 
 def validate_input_args(input_args: list) -> bool:
     """
