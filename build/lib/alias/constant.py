eb0ce6d69801cbc4e1ecd035b4820f9bea884908
nagarajarchak <archak.nagaraj@gmail.com>
2024-06-12 21:38:09 +0530

    Added build files

---
+++ build/lib/alias/constant.py
@@ -0,0 +1,4 @@
+BASH_RC_FILE_NAME = ".bash_profile"
+BASH_ALIAS_FILE_NAME = ".shell-sobriquet-aliases.sh"
+LIST = "ls"
+CLEAR = "clear"
---
+++ build/lib/alias/validate.py
@@ -0,0 +1,19 @@
+import alias.constant as c
+
+def validate_input_args(input_args: list) -> bool:
+    """
+    This function validates input arguments.
+
+    Parameters
+    ----------
+    :param input_args: The input arguments to validate
+    """
+
+    if len(input_args) == 2 and input_args[1] in [c.LIST, c.CLEAR]:
+        return True
+    elif len(input_args) == 3:
+        return True
+    else:
+        print("Invalid input arguments provided.")
+        return False
+    
